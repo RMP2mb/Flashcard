f85fb16af45c1a4fd928e9f8d14eb036ee5881f8
RMP2mb <160626182+RMP2mb@users.noreply.github.com>
2024-10-29 11:45:38 -0300

    29/10

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Flashcard
+# 3trimestre
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do Mundo?',
+    'A Rússia é o maior país do Mundo, a sua área é de 17.100.000 km² e extraordinariamente se extende por dois continentes - Ásia e Europa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do Mundo?',
+    'O continente Asiático é o maior continente do Mundo, a sua área é de 1/3 da extensão do Planeta Terra'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual os dois maiores oceanos no Mundo?',
+    'Oceano Pacífico e Oceano Atlântico são os maiores oceanos do Mundo - o Oceano Pacífico é o dobro do tamanho do Oceano Atlântico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual as três maiores potências do Mundo?',
+    'EUA - Estados Unidos da América, China e Rússia são as maiores potências do Mundo'
+)
